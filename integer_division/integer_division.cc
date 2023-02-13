2e089cbfccef9ac7a26fc55f485e45c33b94b1bf
victor_workspace <victor@aws_workspace>
2023-02-12 18:14:47 -0600

    update division

--- integer_division/integer_division.cc
+++ integer_division/integer_division.cc
@@ -10,13 +10,13 @@ int main ()
   //
 
   cout << setw(5) << "";
-  for (int32_t denom=1; denom<=10; ++denom) {
+  for (int32_t denom=-10; denom<=10; ++denom) {
     cout << setw(4) << showpos << denom;
   }
   cout << endl;
 
   cout << setfill('-') << setw(5) << "";
-  for (int32_t denom=1; denom<=10; ++denom) {
+  for (int32_t denom=-10; denom<=10; ++denom) {
     cout << setfill('-') << setw(4) << "";
   }
   cout << endl;
@@ -26,15 +26,22 @@ int main ()
   // Fill in the table rows.
   //
 
-  for (int32_t numer=1; numer<=10; ++numer)
+  for (int32_t numer=-10; numer<=10; ++numer)
   {
-    cout << setw(3) << showpos << numer << ": ";
-    for (int32_t denom=1; denom<=10; ++denom)
+    cout << setw(3) << numer << ": ";
+    for (int32_t denom=-10; denom<=10; ++denom)
     {
-      int32_t result = numer/denom;
+	    int32_t result = numer/denom;
       cout << setw(4) << result;
-    }
+      try{
+        if(denom == 0){
+          throw denom; //exception for 0 to avoid floating point exception
+        } 
+      }
+      catch(...){
+        cout << setw(4) << " "; //fill in with empty space
+      }
     cout << endl;
+    }
   }
 }
-
