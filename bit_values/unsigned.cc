1073410baf89b9209a15107fefcd890dd1ae5de8
victor_workspace <victor@aws_workspace>
2023-02-20 15:16:16 -0600

    update code + notes

--- bit_values/unsigned.cc
+++ bit_values/unsigned.cc
@@ -12,7 +12,17 @@ int main ()
   ///////////////////////////////////
   // SET THE APPROPRIATE BITS HERE //
   ///////////////////////////////////
-  
+  x |= (1 << 2); 
+  x |= (1 << 3); 
+  x |= (1 << 5);
+  x |= (1 << 7); 
+  x |= (1 << 11); 
+  x |= (1 << 13);
+  x |= (1 << 17);
+  x |= (1 << 19); 
+  x |= (1 << 23);
+  x |= (1 << 29);
+  x |= (1 << 31); 
 
   cout << "after setting bits:" << endl;
   cout << "  x = " << x << endl;
@@ -20,6 +30,7 @@ int main ()
   ///////////////////////
   // TOGGLE BIT 3 HERE //
   ///////////////////////
+  x ^= (1 << 3); 
 
   cout << "after first bit 3 toggle:" << endl;
   cout << "  x = " << x << endl;
@@ -27,6 +38,7 @@ int main ()
   //////////////////////////////////////
   // REPEAT YOUR PREVIOUS ACTION HERE //
   //////////////////////////////////////
+  x ^= (1 << 3); 
 
   cout << "after second bit 3 toggle:" << endl;
   cout << "  x = " << x << endl;
@@ -34,6 +46,7 @@ int main ()
   //////////////////////
   // CLEAR BIT 7 HERE //
   //////////////////////
+  x &= ~(1 << 7); 
 
   cout << "after clearing bit 7:" << endl;
   cout << "  x = " << x << endl;
@@ -41,6 +54,7 @@ int main ()
   ///////////////////////////
   // CLEAR BITS 16-31 HERE //
   ///////////////////////////
+  x &= ~(0xFFFF0000); 
 
   cout << "after clearing bits 16-31:" << endl;
   cout << "  x = " << x << endl;
