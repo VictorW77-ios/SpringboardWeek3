30b206372ef793c1196968cdb3cc7ff748087790
victor_workspace <victor@aws_workspace>
2023-02-20 12:44:22 -0600

    update code + notes

--- integer_division/integer_division.cc
+++ integer_division/integer_division.cc
@@ -31,17 +31,12 @@ int main ()
     cout << setw(3) << numer << ": ";
     for (int32_t denom=-10; denom<=10; ++denom)
     {
+      if(denom == 0){
+        cout << "x"; 
+      }
 	    int32_t result = numer/denom;
       cout << setw(4) << result;
-      try{
-        if(denom == 0){
-          throw denom; //exception for 0 to avoid floating point exception
-        } 
-      }
-      catch(...){
-        cout << setw(4) << " "; //fill in with empty space
-      }
-    cout << endl;
     }
+    cout << endl;
   }
 }
